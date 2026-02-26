902a7f9466f31fed46c8077d4725e8aec361327c
Pavan C N <p0073100@gmail.com>
2026-02-26 11:45:28 +0530

    Add Unit-1: Program 1a - Input/Output Function
    This program prompts the user to enter a number and then displays the entered number.

---
+++ Unit-1/01_InputOutput.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Program 1a: Input/Output Function
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("You entered %d\n", num);
+    return 0;
+}
+
+/* Output:
+Enter a number: 10
+You entered 10
+*/
